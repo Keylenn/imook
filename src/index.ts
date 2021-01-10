9147fb0fb0e64d510f8593fe8b4e5641aecc633c
Keylenn <darklight_on@163.com>
2021-01-10 22:09:36 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import createLocalStore from './createLocalStore'
+import Provider, {withStores} from './Provider'
+import {ActionUtilOption} from './types/store'
+
+export {createLocalStore, Provider, withStores, ActionUtilOption}
+export default {createLocalStore, Provider, withStores}
