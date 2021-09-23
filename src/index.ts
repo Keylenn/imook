790471ac6c94e091bf98e09ae8270617da38fea9
hejilun <hejilun@joyy.sg>
2021-09-23 18:11:15 +0800

    feat: export immer plugin

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import createLocalStore from './createLocalStore'
 
+export {enableAllPlugins, enableES5, enableMapSet, enablePatches} from 'immer'
+
 export {createLocalStore}
+
 export default createLocalStore
